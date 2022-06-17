70dbd0c173d4d2d572e920e3a8af71ee5c61dc04
pcsoluciones <pcsoluciones.cl@gmail.com>
2022-06-16 23:57:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Crypto from './components/Crypto';
+
+function App() {
+
+  return (
+    <div className="container-fluid">
+      <h1 className='text-center'>React Crypto</h1>
+      <Crypto />
+    </div>
+  );
+}
+
+export default App;
